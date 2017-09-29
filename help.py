fe55dc56730024bd3f83ff7ff66aa7d472fa0cf2
santosg57 <lgs@unam.mx>
2017-09-29 05:27:28 -0500

    Create help.py

---
+++ help.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+import sys
+ 
+print "Número de parámetros: ", len(sys.argv)
+print "Lista de argumentos: ", sys.argv
+
+par = sys.argv[1]
+
+print help(par)
